20d3c473ad04147ca8b39cf80e3102e5c4e9f73f
Jake Roughton <112968037+JakeRoughton@users.noreply.github.com>
2023-03-06 02:05:38 -0500

    Add files via upload
    modified the export in to a function

--- Card.js
+++ Card.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-function Card(props){
+export function Card(props) {
     return (
         <div>
-        <image src={`https://rickandmortyapi.com/api/character/avatar/${id}`} alt="Rick&Morty =P" width='200px' height='200px'/>
+            <image src={`https://rickandmortyapi.com/api/character/avatar/${id}`} alt="Rick&Morty =P" width='200px' height='200px' />
             <div>
-        <h3>{props.name}</h3>
-        <p>{props.gender}</p>
-        <p>{props.species}</p>
-        <p>{props.status}</p>
+                <h3>{props.name}</h3>
+                <p>{props.gender}</p>
+                <p>{props.species}</p>
+                <p>{props.status}</p>
             </div>
         </div>
     )
 }
-
-export default Card;
